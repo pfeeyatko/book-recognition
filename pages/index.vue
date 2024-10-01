1f3a3cc68624a57712681184b8bd90f3dfcff68e
Prajwol Onta <prajwol@year13.com.au>
2024-10-02 09:22:23 +1000

    install dependencies

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Scan </button>
+    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"> Checkout </button>
+  </div>
+</template>
