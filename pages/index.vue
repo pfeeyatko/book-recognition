09f34621c9c17f7bcead9e8c12efc11bc9687143
aydin <aydin.salimov@year13.com.au>
2024-10-02 13:33:43 +1000

    Componentised

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,12 @@
 <script setup>
-const openLibrary = useOpenLibrary();
-const postData = usePostData();
+  import ScanNew from '~/components/ScanNew';
+  import Checkout from '~/components/Checkout';
 
-const isbn = ref("");
-const barcodeId = ref("");
-const openConfirmations = ref(false);
-const book = ref(null);
+  const activeComponent = shallowRef(null);
 
-const refreshBarcodeId = () => {
-  barcodeId.value = useId();
-}
-
-let StreamBarcodeReader
-
-if (process.client) {
-  StreamBarcodeReader = (await import('vue-barcode-reader')).StreamBarcodeReader
-}
-
-const onDecode = async (bookIsbn) => {
-  if(bookIsbn) {
-    openConfirmations.value = true;
-    isbn.value = bookIsbn;
-    // get book details from library
-
-    book.value = await openLibrary.search(bookIsbn);
-    // show modal
-    openConfirmations.value = true;
+  const setActiveComponent = (component) => {
+    activeComponent.value = component;
   }
-}
-
-const saveBookToSheet = () => {
-  // save book to sheet
-  resetBarcode();
-}
-
-const resetBarcode = () => {
-  openConfirmations.value = false;
-  isbn.value = ""
-  book.value = null;
-  refreshBarcodeId();
-}
-
-onMounted(() => {
-  refreshBarcodeId()
-})
 </script>
 <template>
   <div class="min-h-screen bg-gray-100">
@@ -52,17 +15,10 @@ onMounted(() => {
     <div class="container mx-auto px-4 py-8">
       <h1 class="text-4xl font-bold text-center mb-8">Welcome to the POS System</h1>
       <div class="flex justify-center space-x-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Scan</button>
-        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Checkout</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setActiveComponent(ScanNew)">Scan</button>
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" @click="setActiveComponent(Checkout)">Checkout</button>
       </div>
     </div>
-
-    <ClientOnly>
-      <div class="app max-w-[640px] mx-auto">
-        <StreamBarcodeReader @decode="(a) => onDecode(a)" :key="barcodeId"/>
-        Input Value: {{ isbn || "Nothing" }}
-      </div>
-    </ClientOnly>
-    <LazyUIModal v-if="openConfirmations && book" :book="book" @save="saveBookToSheet" @cancel="resetBarcode"/>
+    <component :is="activeComponent"/>
   </div>
 </template>
